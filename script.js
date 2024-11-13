eb65d7b7240e914441171e6ba5a49b4dc8af5690
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-13 19:09:16 -0400

    inserido rota para imprimir titulo

--- script.js
+++ script.js
@@ -19,19 +19,21 @@ const arrayPostagens = [
   },
 ]
 
+console.log(arrayPostagens[1].titulo)
+
 // Criar um elemento
 let article = document.createElement("article")
 console.log(article)
 
 // Manipular um elemento
 article.innerHTML = `
-console.log(article)
 <h3>Pop Vegan</h3>
-      <p class="subtitulo">Comida vegana para todos!</p>
-      <div class="data">06/07/2022</div>
-      <p>Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)</p>
-    </article>
-    `
+<p class="subtitulo">Comida vegana para todos!</p>
+<div class="data">06/07/2022</div>
+<p>Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)</p>
+</article>
+`
+article.id = "post-2"
 
 // Adicionar o elemento na página
 let main = document.querySelector("main")
